e032851b9a7fee68fca14267b80dfbdcf866aaef
davehall <jiuzixue09@hotmail.com>
2020-03-04 22:34:11 +0800

    监听模式测试

---
+++ sniff_mode/WaterHeater.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta, abstractmethod
+
+
+class WaterHeater:
+
+    def __init__(self):
+        self.__observers = []
+        self.__temperature = 25
+
+    def get_temperature(self):
+        return self.__temperature
+
+    def set_temperature(self, temperature):
+        self.__temperature = temperature
+        print("current temperature is:" + str(self.__temperature) + "C")
+        self.notifies()
+
+    def add_observer(self, observer):
+        self.__observers.append(observer)
+
+    def notifies(self):
+        for o in self.__observers:
+            o.update(self)
+
+
+class Observer(metaclass=ABCMeta):
+
+    @abstractmethod
+    def update(self, water_heater):
+        pass
+
+
+class WashingMode(Observer):
+
+    def update(self, water_heater):
+        if 50 <= water_heater.get_temperature() < 70:
+            print("you can wash now.")
+
+
+class DrinkMode(Observer):
+
+    def update(self, water_heater):
+        if water_heater.get_temperature() >= 100:
+            print("you can drink now.")
+
+
+def test_water_heater():
+    heater = WaterHeater()
+    washing_obser = WashingMode()
+    drinking_obser = DrinkMode()
+    heater.add_observer(washing_obser)
+    heater.add_observer(drinking_obser)
+    heater.set_temperature(40)
+    heater.set_temperature(60)
+    heater.set_temperature(100)
+
+
+if __name__ == '__main__':
+    test_water_heater()
